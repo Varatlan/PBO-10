04d3b1ec2168c567743e087ffe622ff0f4a7e0cd
Varatlan <148843870+Varatlan@users.noreply.github.com>
2023-10-29 11:51:43 +0700

    Add files via upload

---
+++ PBO10/build/generated-sources/ap-source-output/UtsPbo/Buku_1_.java
@@ -0,0 +1,16 @@
+package UtsPbo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-10-26T14:43:15")
+@StaticMetamodel(Buku_1.class)
+public class Buku_1_ { 
+
+    public static volatile SingularAttribute<Buku_1, String> tahunTerbit;
+    public static volatile SingularAttribute<Buku_1, String> penerbit;
+    public static volatile SingularAttribute<Buku_1, String> isbn;
+    public static volatile SingularAttribute<Buku_1, String> judulBuku;
+
+}
